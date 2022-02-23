2a2892cc8c65b5a1738e255544c87bdf667bc8e4
Jasper Hwong Yu Chen <jasperhwong0077@gmail.com>
2022-02-23 09:33:51 +0800

    feat: post CRUD with mikroorm and type-graphql

--- src/migrations/resolvers/post.ts
+++ src/migrations/resolvers/post.ts
@@ -1,5 +1,5 @@
 import { Post } from "../../entities/Post";
-import { Resolver, Query, ObjectType, Ctx } from "type-graphql";
+import { Resolver, Query, ObjectType, Ctx, Arg, Int, Mutation } from "type-graphql";
 import { MyContext } from "src/types";
 
 @ObjectType()
@@ -8,5 +8,60 @@ export class PostResolver {
   @Query(() => [Post])
   posts(@Ctx() { em }: MyContext): Promise<Post[]> {
     return em.find(Post, {});
+  }
+
+  @Mutation(() => Post, {  nullable: true  })
+  post(
+    @Arg("id", () => Int) id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    return em.findOne(Post, { _id: id });
+  }
+
+  @Mutation(() => Post, {  nullable: true  })
+  async createPost(
+    @Arg("title") title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    const post = em.create(Post, {
+      createdAt: new Date(),
+      title: title,
+      updatedAt: new Date()
+    });
+    await em.persistAndFlush(post);
+    return post;
+  }
+
+  @Mutation(() => Post, {  nullable: true  })
+  async updatePost(
+    @Arg("id") id: number,
+    @Arg("title") title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    try {
+      const post = await em.findOneOrFail(Post, { _id: id });
+      if (typeof title !== undefined) {
+        post.title = title;
+        await em.persistAndFlush(post);
+      }
+      return post;
+    } catch (e) {
+      console.error('Not found', e);
+      return null;
+    }
+  }
+
+  @Mutation(() => Boolean)
+  async deletePost(
+    @Arg("id") id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<boolean> {
+    try {
+      await em.nativeDelete(Post, { _id: id });
+      return true;
+    } catch (e) {
+      console.log(e);
+      return false;
+    }
   }
 }
