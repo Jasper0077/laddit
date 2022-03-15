43d1f5a3774978710c7f97d59c1fcfdd48b75d14
Jasper Hwong Yu Chen <jasperhwong0077@gmail.com>
2022-03-15 22:48:49 +0800

    feat: add login page and graphql in frontend

--- web/src/pages/login.tsx
+++ web/src/pages/login.tsx
@@ -3,26 +3,23 @@ import { Formik, Form } from 'formik'
 import { Box, Button } from '@chakra-ui/react';
 import { Wrapper } from "../components/Wrapper";
 import { InputField } from '../components/InputField';
-import { useRegisterMutation } from '../generated/graphql';
+import { useLoginMutation } from '../generated/graphql';
 import { toErrorMap } from '../utils/toErrorMap';
 import { useRouter } from "next/router";
-interface registerProps {
 
-}
-
-const Register: React.FC<registerProps> = ({ }) => {
+const Login: React.FC<{}> = ({ }) => {
   const router = useRouter();
-  const [, register] = useRegisterMutation();
+  const [, login] = useLoginMutation();
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
         onSubmit={async (values, {setErrors}) => {
-          const response = await register(values);
-          if (response.data?.register.errors) {
+          const response = await login({ options: values});
+          if (response.data?.login.errors) {
             [{field: 'username', message: 'something wrong'}]
-            setErrors(toErrorMap(response.data.register.errors));
-          } else if (response.data?.register.user) {
+            setErrors(toErrorMap(response.data.login.errors));
+          } else if (response.data?.login.user) {
             //worked
             router.push("/");
           }
@@ -52,7 +49,7 @@ const Register: React.FC<registerProps> = ({ }) => {
               type="submit"
               isLoading={isSubmitting}
               colorScheme="teal"
-            >Register</Button>
+            >Login</Button>
           </Form>
         )}
       </Formik>
@@ -60,4 +57,4 @@ const Register: React.FC<registerProps> = ({ }) => {
   );
 };
 
-export default Register
+export default Login
